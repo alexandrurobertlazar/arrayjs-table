54932532c8b3f790c79d1eab8aedd37cbe1f3287
Alexandru Robert Lazar <alex2000robert@yahoo.com>
2022-05-28 00:39:16 +0100

    feat: Version 1.0.5 from npm

---
+++ Table.js
@@ -0,0 +1,92 @@
+export class Table {
+    constructor(arr) {
+        if (arr[0] && (arr[0].id === undefined || arr[0].data === undefined)) {
+            let normArray = []
+            let i = 0;
+            for (const data of arr) {
+                normArray.push({
+                    id: i,
+                    data: data
+                })
+                i++;
+            }
+            this.select = normArray;
+        } else {
+            this.select = arr
+        }
+    }
+
+    delete(target) {
+        target.select.forEach(targetRow => {
+            this.select = this.select.filter(obj => obj != targetRow)
+        })
+        return { select: this.select }
+    }
+
+    whereId(id) {
+        return new Table(this.select.filter(obj => obj.id === id))
+    }
+
+    whereEquals(column, value) {
+        return new Table(this.select.filter(obj => {
+            if (obj.data[column]) {
+                return obj.data[column] == value
+            }
+            return false
+        }))
+    }
+
+    whereNotEquals(column, value) {
+        return new Table(this.select.filter(obj => {
+            if (obj.data[column]) {
+                return obj.data[column] != value
+            }
+            return false
+        }))
+    }
+
+    insert(columns, data) {
+        let newRow = {};
+        let i = 0;
+        for (let col of columns) {
+            if (this.select[0]) {
+                if (this.select[0].data[col]) {
+                    newRow[col] = data[i];
+                    i++;
+                } else throw new Error('Error -1: Column does not exist on table.')
+            } else {
+                newRow[col] = data[i];
+                i++;
+            }
+        }
+        const lastId = this.select[this.select.length - 1].id;
+        this.select.push({ id: lastId + 1, data: newRow });
+    }
+
+    update(columns, data) {
+        this.select.forEach(row => {
+            let i = 0;
+            for (let col of columns) {
+                if (row.data[col]) {
+                    row.data[col] = data[i];
+                } else throw new Error('Error -2: Column does not exist on a row.')
+                i++;
+            }
+        })
+    }
+
+    updateFirst(columns, data) {
+        for (const row of this.select) {
+            let i = 0;
+            let isColFound = false
+            for (let col of columns) {
+                if (row.data[col]) {
+                    row.data[col] = data[i];
+                    isColFound = true;
+                } else throw new Error('Error -2: Column does not exist on a row.')
+                i++;
+            }
+            if (isColFound) break;
+        }
+    }
+}
